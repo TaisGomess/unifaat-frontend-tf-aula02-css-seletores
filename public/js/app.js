f764f482b6fba12598ffe52afbcafa53db7c074e
TaisGomess <163211069+TaisGomess@users.noreply.github.com>
2025-08-26 21:06:11 -0300

    Adiciona estilos, media query para <aside> e interatividade JS

--- public/js/app.js
+++ public/js/app.js
@@ -7,5 +7,23 @@ liElementCollection.forEach((liElement) => {
 
 
         /** CODAR AQUI */
+
+        // Seleciona todos os <li> da lista
+const listaItens = document.querySelectorAll('ul.demo li');
+
+// Percorre cada item e adiciona o listener de clique
+listaItens.forEach((item) => {
+    item.addEventListener('click', (event) => {
+    // Remove .selected de todos os itens
+    listaItens.forEach((li) => li.classList.remove('selected'));
+
+    // Garante que o <li> clicado é o correto, mesmo que clique em <span> ou <strong>
+    const liClicado = event.currentTarget;
+
+    // Adiciona .selected no item clicado
+    liClicado.classList.add('selected');
+    });
+});
+
     });
 });
